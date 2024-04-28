c5eb6f45a3419eee90b249b7237075c58eb978ea
samiullah35 <148207968+samiullah35@users.noreply.github.com>
2024-04-28 22:33:01 +0500

    Adding Word Counter Project

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Display colorful message
+console.log(chalk.red("\t CODE WITH SAMI - WORDS COUNTER"));
+console.log(chalk.blueBright("=".repeat(50)));
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.bold.green("Enter a sentence")
+    }
+]);
+let Words = answers.sentence.trim().split(" ");
+console.log(chalk.blueBright("=".repeat(50)));
+console.log("sentence words:");
+console.log(Words);
+console.log(`\n - Words Count: ${Words.length}`);
+console.log(chalk.yellow("=".repeat(50)));
